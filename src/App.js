58745a9a692dc9b6f46383c7cfdecd406aad77a7
Mateo Olarte <mateo.olarte8@gmail.com>
2018-03-13 02:37:16 -0500

    create new row based on form and create too object

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      guests: [],
+      firstname: "",
+      lastname: ""
+    }
+
+    this.addGuest = this.addGuest.bind(this)
+    this.addFirstName = this.addFirstName.bind(this)
+    this.addLastName = this.addLastName.bind(this)
+  }
+
+  addGuest(e) {
+    const objID = this.state.guests.length === 0 ? 1 : this.state.guests.length + 1
+
+    this.setState({
+      guests: this.state.guests.concat({ id: objID, firstname: this.state.firstname, lastname: this.state.lastname })
+    })
+
+    e.preventDefault()
+  }
+
+  addFirstName(e) {
+    this.setState({
+      firstname: e.target.value
+    })
+  }
+
+  addLastName(e) {
+    this.setState({
+      lastname: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={ this.addGuest }>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" onChange={ this.addFirstName } name="first-name" />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" onChange={ this.addLastName } name="last-name" />
               </div>
 
               <div className="action">
@@ -30,7 +66,12 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                { this.state.guests.map((guest) => 
+                  <tr key={ guest.id }>
+                    <td>{ guest.firstname }</td>
+                    <td>{ guest.lastname }</td>
+                  </tr>
+                ) }
               </tbody>
             </table>
           </div>
